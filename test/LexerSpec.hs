70f639fc039e9abcdeb54ee4cfefd2f419c8c22a
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-12-12 18:53:31 +0100

    Even more tests

--- test/LexerSpec.hs
+++ test/LexerSpec.hs
@@ -1,19 +1,48 @@
 {-# LANGUAGE OverloadedStrings #-}
 module LexerSpec where
 
-import Test.Hspec
--- import Test.QuickCheck
+import Data.ByteString.Lazy (fromStrict)
+import Data.Text.Encoding (encodeUtf8)
 
 import Language.Rust.Lexer (alexScanTokens, AlexPosn(..))
 import Language.Rust.Token
 
+import Test.Hspec
+-- import Test.QuickCheck
+
 -- | main spec
 spec :: Spec
-spec = describe "Language.Rust.Lexer"
-    tokenSpec
+spec = describe "Language.Rust.Lexer" $ do
+    commentSpec
+    numberLiteralSpec
 
-tokenSpec :: Spec
-tokenSpec = describe "tokenization" $ do
+commentSpec :: Spec
+commentSpec = describe "comment tokenization" $ do
+    it "recognizes comment precedence" $
+        alexScanTokens "asdf // asdf = 0.0" ==
+            [ Token (AlexPn 0 1 1) IdentifierToken "asdf"
+            , Token (AlexPn 4 1 5) SpaceToken " "
+            , Token (AlexPn 5 1 6) CommentToken "// asdf = 0.0"
+            ]
+    it "allows inline comments" $
+        alexScanTokens "asdf /* some comment */ 1" ==
+            [ Token (AlexPn 0 1 1) IdentifierToken "asdf"
+            , Token (AlexPn 4 1 5) SpaceToken " "
+            , Token (AlexPn 5 1 6) CommentToken "/* some comment */"
+            , Token (AlexPn 23 1 24) SpaceToken " "
+            , Token (AlexPn 24 1 25) DecimalLiteralToken "1"
+            ]
+    it "allows multiline comments" $
+        alexScanTokens (fromStrict $ encodeUtf8 "bla /* abc\ndef */ b") ==
+            [ Token (AlexPn 0 1 1) IdentifierToken "bla"
+            , Token (AlexPn 3 1 4) SpaceToken " "
+            , Token (AlexPn 4 1 5) CommentToken "/* abc\ndef */"
+            , Token (AlexPn 17 2 7) SpaceToken " "
+            , Token (AlexPn 18 2 8) IdentifierToken "b"
+            ]
+
+numberLiteralSpec :: Spec
+numberLiteralSpec = describe "number tokenization" $ do
     it "recognizes whitespace" $
         alexScanTokens " \t\n\t" == [Token (AlexPn 0 1 1) SpaceToken " \t\n\t"]
     it "recognizes different integer literals" $
@@ -25,4 +54,16 @@ tokenSpec = describe "tokenization" $ do
             , Token (AlexPn 20 1 21) BinaryLiteralToken "0b1101_010"
             , Token (AlexPn 30 1 31) SpaceToken " "
             , Token (AlexPn 31 1 32) DecimalLiteralToken "1234u32"
+            ]
+    it "recognizes floating pointer literals" $
+        alexScanTokens "1.234 1.34E+3 1.78E-2 1. 1f64" ==
+            [ Token (AlexPn 0 1 1) FloatingLiteralToken "1.234"
+            , Token (AlexPn 5 1 6) SpaceToken " "
+            , Token (AlexPn 6 1 7) FloatingLiteralToken "1.34E+3"
+            , Token (AlexPn 13 1 14) SpaceToken " "
+            , Token (AlexPn 14 1 15) FloatingLiteralToken "1.78E-2"
+            , Token (AlexPn 21 1 22) SpaceToken " "
+            , Token (AlexPn 22 1 23) FloatingLiteralToken "1."
+            , Token (AlexPn 24 1 25) SpaceToken " "
+            , Token (AlexPn 25 1 26) FloatingLiteralToken "1f64"
             ]
